5b16715c913defe4bfda84c927194237266f707d
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-07-13 05:53:14 -0600

    @ionic-native/file/ngx

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { saveAs } from 'file-saver';
+import axios from 'axios';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -13,9 +15,10 @@ export class Tab1Page {
   };
   fileName = '';
 
-  constructor() {}
+  constructor(private file: File) {}
 
   async submitForm() {
+    console.log('Método con file saver');
     try {
       this.fileName = 'test save 1';
       console.log('URL enviada:', this.formData.url);
@@ -25,6 +28,28 @@ export class Tab1Page {
         saveAs(blob, this.fileName);
         resolve();
       });
+      console.log('Archivo PDF descargado y guardado correctamente.');
+    } catch (error) {
+      console.error('Error al descargar o guardar el archivo PDF:', error);
+    }
+
+    console.log('Método con axios');
+    try {
+      this.fileName = 'testSave1.pdf';
+      console.log('URL enviada:', this.formData.url);
+      const response = await axios({
+        url: this.formData.url,
+        method: 'GET',
+        responseType: 'blob',
+      });
+
+      const filePath = this.file.dataDirectory;
+
+      await this.file.writeFile(filePath, this.fileName, response.data, {
+        replace: true,
+      });
+      //filePath, response.data, { replace: true }
+
       console.log('Archivo PDF descargado y guardado correctamente.');
     } catch (error) {
       console.error('Error al descargar o guardar el archivo PDF:', error);
